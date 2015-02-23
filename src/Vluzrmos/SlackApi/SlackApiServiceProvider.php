bd702cc8b23263ba5e177dd18be4356e54c22847
Vagner do Carmo <vluzrmos@gmail.com>
2015-02-23 01:52:54 -0300

    Initial workbench

---
+++ src/Vluzrmos/SlackApi/SlackApiServiceProvider.php
@@ -0,0 +1,34 @@
+<?php namespace Vluzrmos\SlackApi;
+
+use Illuminate\Support\ServiceProvider;
+
+class SlackApiServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return [];
+	}
+
+}
