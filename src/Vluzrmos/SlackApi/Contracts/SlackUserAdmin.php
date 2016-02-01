cc1cd95f4696b3d4ee0869cfaeba1bfdb1677c23
Anthony Lawrence <freelancer@anthonylawrence.me.uk>
2016-02-01 00:02:29 +0000

    Update SlackUserAdmin.php
    Added setRegular, setInactive and setAdmin to the contract.

--- src/Vluzrmos/SlackApi/Contracts/SlackUserAdmin.php
+++ src/Vluzrmos/SlackApi/Contracts/SlackUserAdmin.php
@@ -5,4 +5,7 @@ namespace Vluzrmos\SlackApi\Contracts;
 interface SlackUserAdmin
 {
     public function invite($email, $options = []);
+    public function setRegular($user);
+    public function setAdmin($user);
+    public function setInactive($user);
 }
