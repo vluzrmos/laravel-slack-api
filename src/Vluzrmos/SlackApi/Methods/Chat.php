c1efaa96f9dacf26073bc0d85c9f9ad7bbd4f86a
Dannel Albert <dannelalbert@WorkBookBro.local>
2017-09-11 12:07:40 -0400

    + Ability to update options e.g. attachments in a message.

--- src/Vluzrmos/SlackApi/Methods/Chat.php
+++ src/Vluzrmos/SlackApi/Methods/Chat.php
@@ -74,11 +74,26 @@ class Chat extends SlackMethod implements SlackChat
      * @param string $channel Channel containing the message to be updated.
      * @param string $text New text for the message, using the default formatting rules.
      * @param int|string $ts Timestamp of the message to be updated.
+     * @param array $options
      *
+     * @example
+     * <pre>
+     * [
+     *    "username" => "My Bot", //Name of bot.
+     *    "as_user"  => true, //Pass true to post the message as the authed user, instead of as a bot
+     *    "parse"    => "full", //Change how messages are treated. See below.
+     *    "link_names" => 1, //Find and link channel names and usernames.
+     *    "attachments" => ["pretext" => "pre-hello", "text" => "text-world"], //Structured message attachments.
+     *	  "unfurl_links" => true, //Pass true to enable unfurling of primarily text-based content.
+     *    "unfurl_media" => true, //Pass false to disable unfurling of media content.
+     *    "icon_url" => "http://lorempixel.com/48/48", //URL to an image to use as the icon for this message
+     *    "icon_emoji"=> ":chart_with_upwards_trend:" //emoji to use as the icon for this message. Overrides icon_url.
+     * ]
+     *</pre>
      * @return array
      */
-    public function update($channel, $text, $ts)
+    public function update($channel, $text, $ts, $options = [])
     {
-        return $this->method('update', compact('channel', 'text', 'ts'));
+        return $this->method('update', array_merge(compact('channel', 'text', 'ts'), $options));
     }
 }
